3889e1df69eb972605d6e7535556f01578d5a4f3
chaemin58 <sulv0309@naver.com>
2026-03-30 17:24:18 +0900

    feature: layout.js에 header컴포넌트 추가

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,12 @@
+import Header from '@/components/Header';
 import './globals.css';
 
 export default function RootLayout({ children }) {
   return (
     <html lang="ko">
       <body>
-        {children}
+        <Header />
+        <div style={{ padding: '16px' }}>{children}</div>
       </body>
     </html>
   );
