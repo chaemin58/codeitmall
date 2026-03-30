88efbd6f86d31e8ab74cff4c9bb3dd1003ea311b
chaemin58 <sulv0309@naver.com>
2026-03-30 16:53:23 +0900

    page.js 추가

---
+++ app/products/[id]/components/ProductInfo.js
@@ -0,0 +1,10 @@
+export default function ProductInfo(){
+  return(
+    <div>
+      <h1>화이트 코드잇 스웨트 셔츠</h1>
+      <p>코드잇 / PK3434</p>
+      <p>26,900원</p>
+      <p>포인트 적립 1,614</p>
+    </div>
+  )
+}
