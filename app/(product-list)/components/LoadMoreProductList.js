33212ee13e8b0bb05093c4e5ab202fbe9490ec43
chaemin58 <sulv0309@naver.com>
2026-04-01 20:55:05 +0900

    feature: 상품 더보기 버튼 클릭 이벤트 함수 작성

--- app/(product-list)/components/LoadMoreProductList.js
+++ app/(product-list)/components/LoadMoreProductList.js
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from "react";
 import ProductList from "./ProductList";
+import { get } from "@/lib/fetch";
 
 export default function LoadMoreProductList({
   initialProducts = [],
@@ -9,19 +10,35 @@ export default function LoadMoreProductList({
 }){
   const [products, setProducts] = useState(initialProducts);
   const [next, setNext] = useState(initialNext);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   //클릭하면 다음 데이터를 불러오는 함수
-  const handleLoadMore = () => {
+  const  handleLoadMore = async() => {
+    try{
+      setLoading(true);
+      const {results: moreProducts, next: nextUrl} =  await get(next);
+      setProducts((prev) => [...prev, ...moreProducts]);
+      setNext(nextUrl);
+    } catch(err){
+      setError(err);
+    } finally{
+      setLoading(false);
+    }
     
   }
   return(
     <div>
       <ProductList products={products} />
       {next && (
-        <button style={{marginTop: '20px'}} onClick ={handleLoadMore}>
+        <button 
+          style={{marginTop: '20px'}}
+          onClick ={handleLoadMore}
+          disabled={loading}>
         상품 더 보기
         </button>
       )}
+      {error && <div>상품을 더 불러오는데 실패했습니다.</div>}
     </div>
   )
 }
