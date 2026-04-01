e098a1a7cccbe20971db1582b18b8bb974e8b7be
chaemin58 <sulv0309@naver.com>
2026-04-01 21:33:31 +0900

    refactor: 원래는 설정페이지 전체에서 useTheme 훅을 사용하며 클라이언트 컴포넌트였으나 ThemeSelect 컴포넌트를 추가하며 자바스크립트 번들 크기를 줄임 - settings 폴더에 components 폴더 추가 - components 폴더에 ThemeSelect.js 파일 추가

--- app/settings/page.js
+++ app/settings/page.js
@@ -1,5 +1,10 @@
-'use client';
+import ThemeSelect from "./components/ThemeSelect";
 
 export default function Settings() {
-  return <h1>설정 페이지</h1>;
+  return (
+    <div>
+      <h1 style={{ marginBottom: '20px' }}>설정 페이지</h1>
+      <ThemeSelect />
+    </div>
+  )
 }
