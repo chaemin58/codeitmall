650ac84cf821decf1accbcdc6940d120ae2d98b7
chaemin58 <sulv0309@naver.com>
2026-04-07 00:53:13 +0900

    style: 바디에 다운받은 폰트(notoSansKR) 적용

--- app/layout.js
+++ app/layout.js
@@ -4,11 +4,17 @@ import Header from '@/components/Header';
 import styles from './layout.module.css';
 import './globals.css';
 import { ThemeProvider } from './contexts/ThemeContext';
+import { Noto_Sans_KR } from 'next/font/google';
+
+const notoSansKR = Noto_Sans_KR({
+  preload: false,
+  weight: ['400','500','700']
+})
 
 export default function RootLayout({ children }) {
   return (
     <html lang="ko">
-      <body>
+      <body className={notoSansKR}>
         <ThemeProvider>
           <Header />
           <div className={styles.container}>{children}</div>
